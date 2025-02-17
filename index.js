c6be197c2ebea883150e09b02acebf0eff0812e0
abhinav <abhinavpp4326@gmail.com>
2025-02-17 11:34:39 +0530

    episode -1

---
+++ index.js
@@ -0,0 +1,14 @@
+const parent=React.createElement(
+  "div",
+    {id:"parent"},
+    React.createElement(
+      "div",
+      {id:"child"},
+      [
+      React.createElement("h1",{},"i am h1 tag"),
+      React.createElement("h1",{},"this is my h2 tag")
+      ]
+    )
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
